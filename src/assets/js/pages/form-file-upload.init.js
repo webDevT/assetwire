17e594d8cc09ed5c5f8aa02aa4f17f0dd27a833d
webDevT <karinadesyatnik27@gmail.com>
2024-01-26 17:50:40 +0200

    new pages

--- src/assets/js/pages/form-file-upload.init.js
+++ src/assets/js/pages/form-file-upload.init.js
@@ -7,6 +7,33 @@ File: Form file upload Js File
 */
 
 // Dropzone
+
+var dropzonePreviewNode1 = document.querySelector("#dropzone-preview-list1");
+dropzonePreviewNode1.id = "";
+if (dropzonePreviewNode1) {
+    var previewTemplate1 = dropzonePreviewNode1.parentNode.innerHTML;
+    dropzonePreviewNode1.parentNode.removeChild(dropzonePreviewNode1);
+    var dropzone1 = new Dropzone("#dropzone1", {
+        url: 'https://httpbin.org/post',
+        method: "post",
+        previewTemplate: previewTemplate1,
+        previewsContainer: "#dropzone-preview1",
+    });
+}
+
+var dropzonePreviewNode2 = document.querySelector("#dropzone-preview-list2");
+dropzonePreviewNode2.id = "";
+if (dropzonePreviewNode2) {
+    var previewTemplate2 = dropzonePreviewNode2.parentNode.innerHTML;
+    dropzonePreviewNode2.parentNode.removeChild(dropzonePreviewNode2);
+    var dropzone2 = new Dropzone("#dropzone2", {
+        url: 'https://httpbin.org/post',
+        method: "post",
+        previewTemplate: previewTemplate2,
+        previewsContainer: "#dropzone-preview2",
+    });
+}
+
 var dropzonePreviewNode = document.querySelector("#dropzone-preview-list");
 dropzonePreviewNode.id = "";
 if (dropzonePreviewNode) {
